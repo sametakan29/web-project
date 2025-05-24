5f28d876f54c830c6f293df5f34caf18d2167a67
Abdülsamet Akan <akansametgs@gmail.com>
2025-05-24 23:47:36 +0300

    son

--- login.php
+++ login.php
@@ -7,31 +7,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     // 2. Form verilerini al
     if (isset($_POST['username'])) 
-    {
-    $kullaniciAdi = $_POST['username'];
-    } 
-    else 
-    {
-    $kullaniciAdi = '';
-    }
+    {$kullaniciAdi = $_POST['username'];} 
+    else {$kullaniciAdi = '';}
 
-    if (isset($_POST['password'])) 
-    {
-    $sifre = $_POST['password'];
-    } 
+    if (isset($_POST['password']))
+    {$sifre = $_POST['password']; } 
     else 
-    {
-    $sifre = '';
-    }
+    {$sifre = '';}
 
     // 6. Kullanıcı adı ve şifreyi karşılaştır
     if ($kullaniciAdi === $dogruKullanici && $sifre === $dogruSifre) {
         // Başarılı giriş
         echo "Hoşgeldiniz " . htmlspecialchars($kullaniciAdi);
         echo " Şifreniz ".htmlspecialchars($sifre);
-    } else {
+    } 
+    else {
         // Başarısızsa tekrar login sayfasına yönlendir
-        header("Location: login.html");
+        header("Location:login.html");
         exit();
     }
 }
